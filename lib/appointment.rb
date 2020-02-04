441e0500221671d9f803b2d89224907b645c61f4
Bruno Silva <brunosilvinosilva@gmail.com>
2020-02-04 15:20:45 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,19 @@
+class Appointment 
+
+    attr_accessor :patient, :date, :doctor 
+
+    @@all = []
+
+    def initialize(patient, date, doctor)
+        @patient = patient 
+        @date = date 
+        @doctor = doctor 
+
+        @@all << self
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+end 
